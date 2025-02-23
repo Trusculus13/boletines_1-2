94141d6574ca03b5cc17e08069e744d15033de27
Gabriel <gabrielgk2003@gmail.com>
2025-02-23 21:48:03 +0100

    Ejercicio 7 del boletin 1

---
+++ Boletin_1/Ej7.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Pedir al usuario dos frases y juntalas. Mostrar el resultado por pantalla.
+
+__author__ = "Gabriel Gonzalez Kowalska"
+
+frase1 = input("Introduce una frase: ")
+
+frase2 = input("Introduce otra frase: ")
+
+print("La union de las dos frases es: ", frase1 + " " + frase2)
