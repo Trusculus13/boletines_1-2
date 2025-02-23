96ad7705982627658e31fdf32f5f974baf1376cd
Gabriel <gabrielgk2003@gmail.com>
2025-02-23 21:41:35 +0100

    Ejercicio 5 del boletin 1

---
+++ Boletin_1/Ej5.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Pedir al usuario dos números enteros y dividirlos. Mostrar el resultado por pantalla.
+
+__author__ = "Gabriel Gonzalez Kowalska"
+
+num1 = int(input("Introduce un numero entero: "))
+
+num2 = int(input("Introduce otro numero entero: "))
+
+print(f"La division de {num1} y {num2} es:", num1 / num2)
