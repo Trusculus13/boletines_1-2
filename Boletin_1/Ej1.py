97a00e7b011b31fe2a6e3b51fa9d87b5d70a35ff
Gabriel <gabrielgk2003@gmail.com>
2025-02-23 21:27:01 +0100

    Ejercicio 1 del boletin 1

---
+++ Boletin_1/Ej1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Pedir al usuario dos números enteros y sumarlos. Mostrar el resultado por pantalla.
+
+__author__ = "Gabriel Gonzalez Kowalska"
+
+
+num1 = int(input("Introduce un número entero: "))
+
+num2 = int(input("Introduce otro número entero: "))
+
+print("La suma es: ", num1 + num2)
