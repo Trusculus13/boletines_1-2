d224e6bb02c91157681ee548a1f513c7dad38da2
Gabriel <gabrielgk2003@gmail.com>
2025-02-23 21:34:27 +0100

    Ejercicio 3 del boletin 1

---
+++ Boletin_1/Ej3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Pedir al usuario dos números enteros y multiplicarlos. Mostrar el resultado por pantalla.
+
+__author__ = "Gabriel Gonzalez Kowalska"
+
+num1 = int(input("Introduce un numero entero: "))
+
+num2 = int(input("Introduce otro numero entero: "))
+
+print(f"La multiplicacion de {num1} y {num2} es: {num1 * num2}")
